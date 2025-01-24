1b04c68e0d9b211cce402cdd3b59a98098e9b892
Gal <68018569+CraziestOwl@users.noreply.github.com>
2025-01-24 11:00:49 +0200

    - fix nil error when encountering empty lines in the .env files - ignore commented lines in the .env files (lines that start with #) - strip trailing and leading whitespaces from the completion items

--- lua/cmp_env/init.lua
+++ lua/cmp_env/init.lua
@@ -14,7 +14,15 @@ source.cache = {}
 local read = function(file)
   if vim.fn.filereadable(file) == 1 then
     return with(open(file), function(reader)
-      return vim.split(reader:read("*a"):gsub("export ", ""), "\n", { trimempty = true })
+      return vim.tbl_filter(function(line)
+        if line then
+          -- Ignore empty lines and comments(starting with #)
+          line = line:gsub("^%s*", "")
+          return not (line == "" or vim.regex("^#"):match_str(line))
+        else
+          return false
+        end
+      end, vim.split(reader:read("*a"):gsub("export%s*", ""), "\n", { trimempty = true }))
     end)
   end
   return {}
@@ -28,13 +36,13 @@ end
 ---@param callback function
 source.complete = function(self, _, callback)
   local bufnr = vim.api.nvim_get_current_buf()
-  local items = {}
   if not self.cache[bufnr] and vim.bo.filetype ~= "direnv" then
+    local items = {}
     local files = vim.tbl_extend('keep', vim.fn.glob(".env*", false, true), { os.getenv("DIRENV_FILE") })
     if not vim.tbl_isempty(files) then
       for _, file in ipairs(files) do
         for _, line in ipairs(read(file)) do
-          local k, v = line:match("(.-)=(.+)")
+          local k, v = line:match("%s*(.-)=(.+)%s*")
           if vim.regex("TOKEN\\|KEY\\|SECRET"):match_str(k) then
             v = vim.fn.substitute(v, "[^\\*]", "*", "g")
           end
@@ -49,10 +57,8 @@ source.complete = function(self, _, callback)
       end
     end
     self.cache[bufnr] = items
-    callback(items)
-  else
-    callback(self.cache[bufnr])
   end
+  callback(self.cache[bufnr])
 end
 
 source.get_trigger_characters = function()
